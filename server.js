2844c42d67bd7504b65d687cfc662a41bb91b4e9
omkaryeole10 <omkaryeole10@gmail.com>
2023-12-09 19:04:42 -0700

    login and signup page
    completed login and signup page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const task_management_routes = require('./src/task_management/routes');
 
 const app = express();
-const port = 3000;
+const port = 3300; // ##### PORT CHANGED FROM 3000 to 3300
 
 // Will allow us to get json from our endpoints 
 app.use(express.json())
